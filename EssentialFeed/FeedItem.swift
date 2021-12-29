44b713807ea20a86862fbd485784d35fa2340bad
Jihad Mahmoud <jihad.mahmoud@dnata.com>
2021-12-29 10:37:08 +0400

    Defined the FeedItem model contract

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Jihad Mahmoud on 29/12/2021.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
