74ce71e647cde2c6b81aef5d2c223c314b7e0390
Jihad Mahmoud <jihad.mahmoud@dnata.com>
2022-01-11 15:24:06 +0400

    Deliver invalidData error on 200 HTTP Response with invalid JSON

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -64,6 +64,17 @@ class RemoteFeedLoaderTests: XCTestCase {
         }
     }
     
+    func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
+        let (sut, client) = makeSUT()
+        
+        var capturedErrors = [RemoteFeedLoader.Error]()
+        sut.load { capturedErrors.append($0) }
+        
+        let invalidData = Data("invalid JSON".utf8)
+        client.complete(withStatusCode: 200, data: invalidData)
+        
+        XCTAssertEqual(capturedErrors, [.invalidData])
+    }
     
     // MARK: - Helper
     private func makeSUT(url: URL = URL(string: "https://youtube.com/")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
@@ -87,9 +98,9 @@ class RemoteFeedLoaderTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int, at index: Int = 0) {
+        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
             let response = HTTPURLResponse(url: requestedURLs[index], statusCode: code, httpVersion: nil, headerFields: nil)!
-            messages[index].completion(.success(response))
+            messages[index].completion(.success(data, response))
         }
     }
 
